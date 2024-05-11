79733b84188419e8828d72c8f996e6a8539b5f23
Ana.G. <gochashvili.ana@gmail.com>
2024-05-11 19:24:07 +0400

    M1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,8 @@
 package org.example;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
-        System.out.printf("commit M3 M3 ");
 
-        for (int i = 0; i <= 10; i++) {
-            System.out.println("i = " + i);
-        }
-    }
+    String m1 = "M1";
+
+
 }
